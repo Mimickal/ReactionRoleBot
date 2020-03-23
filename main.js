10a9d4ed9c45e15e74e17df7584be6e4e702b7fd
Mia Moretti <mimickal.dev@gmail.com>
2020-03-22 22:18:01 -0400

    Barebones select message

--- main.js
+++ main.js
@@ -8,10 +8,49 @@ const client = new Discord.Client();
 const token_file = process.argv[2] || '/etc/discord/ReactionRoleBot/token';
 const token = fs.readFileSync(token_file).toString().trim();
 
+let selectedMessage;
+
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}`));
+client.on('message', onMessage);
 
 client.login(token).catch(err => {
-	console.error(err);
-	process.exitCode = 1;
+	logError(err);
+	process.exit(1);
 });
 
+function onMessage(msg) {
+	// TODO warn for ambiguity on multiple mentions
+	if (!msg.mentions.has(client.user)) {
+		return;
+	}
+
+	let msgParts = msg.content.split(/\s+/);
+	msgParts.shift(); // Pop off mention
+	let cmd = msgParts.shift();
+
+	switch (cmd) {
+		case 'select': return selectMessage(msg, msgParts); break;
+		default: logError('Unrecognized command ' + cmd);
+	}
+}
+
+function selectMessage(msg, parts) {
+	// TODO handle these missing
+	// TODO handle these not being an ID
+	// TODO handle these not being a VALID ID
+	let channelId = parts.shift();
+	let messageId = parts.shift();
+
+	client.channels.fetch(channelId)
+		.then(channel => channel.messages.fetch(messageId))
+		.then(message => {
+			// TODO cache this better
+			selectedMessage = message;
+			// TODO print a confirmation
+		})
+		.catch(logError);
+}
+
+function logError(err) {
+	console.error(err);
+}
