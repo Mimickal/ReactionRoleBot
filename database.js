886e9e5ae12f9cd98139eb2d43d9ab083d7f5868
Mia Moretti <mimickal.dev@gmail.com>
2020-03-25 01:51:44 -0400

    Function for getting role mapping from database

--- database.js
+++ database.js
@@ -41,10 +41,33 @@ function removeRoleReact(args) {
 	return knex(REACTS).where(fields).del();
 }
 
+/**
+ * Returns the emoji->role mapping for the given message as a Map object, or
+ * null if the given message has no react roles set up.
+ */
+function getRoleReacts(message_id) {
+	return knex(REACTS)
+		.select(['emoji_id', 'role_id'])
+		.where('message_id', message_id)
+		.then(pairArray => {
+			let mapping = pairArray.reduce(
+				(map, pair) => map.set(pair.emoji_id, pair.role_id),
+				new Map()
+			);
+
+			if (mapping.size === 0) {
+				mapping = null;
+			}
+
+			return Promise.resolve(mapping);
+		});
+}
+
 module.exports = {
 	DISCORD_ID_LENGTH,
 	REACTS,
 	addRoleReact,
-	removeRoleReact
+	removeRoleReact,
+	getRoleReacts
 };
 
